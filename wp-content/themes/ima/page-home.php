7b43ac77cf981ac1fd193bac3b14a807a7332cf7
Margaret Godowns <margaret@fuzzco.com>
2016-12-11 18:21:55 -0500

    more homepage things

--- wp-content/themes/ima/page-home.php
+++ wp-content/themes/ima/page-home.php
@@ -1,6 +1,10 @@
 <?php while (have_posts()) : the_post();
   $header = get_field('header');
   $content = get_field('content');
+  $cta_header = get_field('cta_header');
+  $button_text = get_field('button_text');
+  $button_url = get_field('button_url');
+  $background_image = get_field('contact_background_image');
 ?>
   <?php get_template_part('templates/content', 'page'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
@@ -36,15 +40,22 @@
       ?>
 
       <div class="member">
+        <a href="<?php echo the_field('url'); ?>">
 
-        <?php the_post_thumbnail(); ?>
+          <?php the_post_thumbnail(); ?>
 
+        </a>
       </div>
 
       <?php endwhile; ?>
 
     </div>
-
+    <div class="background-image-section cta aspect-ratio-sixteen-three" style="background-image: url(<?php echo $background_image; ?>);">
+      <div class="content">
+        <h2><?php echo $cta_header; ?></h2>
+        <a class="cta-button" href="<?php echo $button_url; ?>"><?php echo $button_text; ?></a>
+      </div>
+    </div>
   </div>
 
 <?php endwhile; ?>
--- wp-content/themes/ima/templates/background-image-section.php
+++ wp-content/themes/ima/templates/background-image-section.php
@@ -1,4 +1,4 @@
-<div class="background-image-section aspect-ratio-sixteen-four" style="background-image: url(<?php echo the_field('background_image'); ?>);">
+<div class="background-image-section aspect-ratio-sixteen-five" style="background-image: url(<?php echo the_field('background_image'); ?>);">
   <div class="content">
     <h4><?php the_field('overlay_text'); ?></h4>
   </div>
