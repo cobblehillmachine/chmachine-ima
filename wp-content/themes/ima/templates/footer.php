ebf1204fb911a33f69cddbe77ae46cc7546c500d
Margaret Godowns <margaret@fuzzco.com>
2016-12-12 19:07:22 -0500

    home page except map woooo

--- wp-content/themes/ima/templates/footer.php
+++ wp-content/themes/ima/templates/footer.php
@@ -1,5 +1,17 @@
-<footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+<footer>
+  <div class="footer-main">
+    <div class="wrap">
+      <img src="/wp-content/themes/ima/assets/images/footer-logo.png">
+      <?php
+        $number =  get_field('phone_number', 'option');
+        $number_stripped = str_replace(".", "", $number);
+      ?>
+      <a href="tel:<?php echo $number_stripped; ?>"><?php echo $number; ?></a>
+      <a href="mailto:<?php the_field('email', 'option'); ?>"><?php the_field('email', 'option'); ?></a>
+      <a href="<?php the_field('google_maps_link', 'option'); ?>"><?php the_field('address', 'option'); ?></a>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <?php echo date("Y") ?> &copy; 2017 © Independent Marketing Alliance. All Rights Reserved.
   </div>
 </footer>
