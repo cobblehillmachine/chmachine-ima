53fc16e02797da4a21dc8e44209e5219c2ca4464
Margaret Godowns <margaret@fuzzco.com>
2016-12-13 23:13:26 -0500

    a few more thangs

--- wp-content/themes/ima/page-suppliers.php
+++ wp-content/themes/ima/page-suppliers.php
@@ -26,4 +26,37 @@
 
   <?php get_template_part('templates/background-image-section'); ?>
 
+  <div class="main">
+    <div class="full-width-text-block">
+      <h2>Case Studies</h2>
+    </div>
+    <div class="benefits">
+    <div class="case-study-section">
+
+      <?php
+        $args = array(
+            'post_type'    => array(
+                'case_study'
+            ),
+            'posts_per_page' => 3,
+            'orderby' => 'menu_order'
+         );
+        $loop = new WP_Query( $args );
+        while ( $loop->have_posts() ) : $loop->the_post();
+      ?>
+
+      <div class="case-study">
+
+        <a href="<?php the_permalink(); ?>">
+
+          <?php the_post_thumbnail(); ?>
+
+        </a>
+      </div>
+
+      <?php endwhile; ?>
+
+      </div>
+  </div>
+
 <?php endwhile; ?>
