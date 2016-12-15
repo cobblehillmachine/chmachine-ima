6be18ccffe3aff360b82b27c2d05d761653f99ac
Margaret Godowns <margaret@fuzzco.com>
2016-12-14 21:35:46 -0500

    editing top image

--- wp-content/themes/ima/templates/hero.php
+++ wp-content/themes/ima/templates/hero.php
@@ -1,5 +1,5 @@
 <div class="hero aspect-ratio-sixteen-nine" >
-  <video autoplay muted style="width: 100%; height: auto; position: absolute; top: 0; z-index:-1">
+  <video autoplay muted loop style="width: 100%; height: auto; position: absolute; top: 0; z-index:-1">
     <source src="<?php echo the_field('hero_video'); ?>" type="">
     <img src="<?php echo the_field('hero_image'); ?>">
   </video>
