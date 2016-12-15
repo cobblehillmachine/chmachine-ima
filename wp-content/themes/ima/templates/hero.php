40796517d000f55a428bf8bfd274677deb708dab
Margaret Godowns <margaret@fuzzco.com>
2016-12-14 21:09:40 -0500

    video!

--- wp-content/themes/ima/templates/hero.php
+++ wp-content/themes/ima/templates/hero.php
@@ -1,2 +1,6 @@
-<div class="hero aspect-ratio-sixteen-seven" style="background-image: url(<?php echo the_field('hero_image'); ?>);">
+<div class="hero aspect-ratio-sixteen-nine" >
+  <video autoplay muted style="width: 100%; height: auto; position: absolute; top: 0; z-index:-1">
+    <source src="<?php echo the_field('hero_video'); ?>" type="">
+    <img src="<?php echo the_field('hero_image'); ?>">
+  </video>
 </div>
