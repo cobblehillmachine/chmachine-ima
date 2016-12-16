9f952c55db0eff17dd7c2d2aa4970ce7f257bb47
Margaret Godowns <mgodowns@MardiGrasWrasse.local>
2016-12-16 18:59:50 -0500

    qa about page text issues

--- wp-content/themes/ima/gulpfile.js
+++ wp-content/themes/ima/gulpfile.js
@@ -245,7 +245,7 @@ gulp.task('clean', require('del').bind(null, [path.dist]));
 gulp.task('watch', function() {
   browserSync.init({
     files: ['{lib,templates}/**/*.php', '*.php'],
-    proxy: config.devUrl,
+    proxy: 'test.dev:8888',
     snippetOptions: {
       whitelist: ['/wp-admin/admin-ajax.php'],
       blacklist: ['/wp-admin/**']
