30e0e0a79ae508eec73211a20cf705d9a2616575
Margaret Godowns <margaret@fuzzco.com>
2016-12-15 21:33:39 -0500

    all the case studies

--- wp-content/themes/ima/single-case_study.php
+++ wp-content/themes/ima/single-case_study.php
@@ -45,7 +45,47 @@
 
 
         <li>
+          <!-- graph icon -->
+            <?php if ($type === 'financial increase') : ?>
+            <img class="result-icon" src="/wp-content/themes/ima/assets/images/graph.png">
+            <?php elseif ($type === 'increased quality') : ?>
+            <img class="result-icon" src="/wp-content/themes/ima/assets/images/graph.png">
+            <?php elseif ($type === 'financial growth') : ?>
+            <img class="result-icon" src="/wp-content/themes/ima/assets/images/graph.png">
+            <?php elseif ($type === 'greater leverage') : ?>
+            <img class="result-icon" src="/wp-content/themes/ima/assets/images/graph.png">
+            <?php elseif ($type === 'business growth') : ?>
+            <img class="result-icon" src="/wp-content/themes/ima/assets/images/graph.png">
+            <?php elseif ($type === 'market equity') : ?>
+            <img class="result-icon" src="/wp-content/themes/ima/assets/images/graph.png">
+            <?php elseif ($type === 'focused sales force') : ?>
+            <img class="result-icon" src="/wp-content/themes/ima/assets/images/graph.png">
 
+          <!-- carrot icon -->
+            <?php elseif ($type === 'product variety') : ?>
+            <img class="result-icon" src="/wp-content/themes/ima/assets/images/carrot.png">
+            <?php elseif ($type === 'market access') : ?>
+            <img class="result-icon" src="/wp-content/themes/ima/assets/images/carrot.png">
+            <?php elseif ($type === 'knowledge capital') : ?>
+            <img class="result-icon" src="/wp-content/themes/ima/assets/images/carrot.png">
+
+          <!-- start icon -->
+            <?php elseif ($type === 'quality improvement') : ?>
+            <img class="result-icon" src="/wp-content/themes/ima/assets/images/star.png">
+            <?php elseif ($type === 'created opportunities') : ?>
+            <img class="supplier engagement" src="/wp-content/themes/ima/assets/images/star.png">
+
+          <!-- handshake icon -->
+          <?php elseif ($type === 'reliable partnership') : ?>
+          <img class="result-icon" src="/wp-content/themes/ima/assets/images/handshake.png">
+
+          <!-- lightbulb icon -->
+            <?php elseif ($type === 'long term partnership') : ?>
+            <img class="result-icon" src="/wp-content/themes/ima/assets/images/handshake.png">
+            <?php elseif ($type === 'support') : ?>
+            <img class="result-icon" src="/wp-content/themes/ima/assets/images/handshake.png">
+
+          <?php endif; ?>
           <?php echo $type; ?>
 
           <span><?php echo $text; ?></span>
