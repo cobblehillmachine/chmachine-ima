06580d0ba05d47c09d20dde0ee461af66ce75cad
Margaret Godowns <margaret@fuzzco.com>
2016-12-15 19:10:17 -0500

    brands page

--- wp-content/themes/ima/templates/inner-hero.php
+++ wp-content/themes/ima/templates/inner-hero.php
@@ -1,4 +1,4 @@
-<div class="hero aspect-ratio-<?php if (is_page('about')):?>sixteen-six<?php elseif (is_page(array('exclusive_brands', 'news'))): ?>sixteen-three<?php else: ?>sixteen-four<?php endif; ?>" style="background-image: url(<?php echo the_field('hero_background_image'); ?>);">
+<div class="hero aspect-ratio-<?php if (is_page('about')):?>sixteen-six<?php else: ?>sixteen-three<?php endif; ?>" style="background-image: url(<?php echo the_field('hero_background_image'); ?>);">
   <div class="content">
 
     <?php echo the_field('hero_text'); ?>
