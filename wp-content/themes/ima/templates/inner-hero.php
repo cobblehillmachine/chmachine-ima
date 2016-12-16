0c533b27f4c3d7d9fe3d07b40f88a7933701089f
Margaret Godowns <margaret@fuzzco.com>
2016-12-15 19:27:02 -0500

    distributors

--- wp-content/themes/ima/templates/inner-hero.php
+++ wp-content/themes/ima/templates/inner-hero.php
@@ -1,4 +1,4 @@
-<div class="hero aspect-ratio-<?php if (is_page('about')):?>sixteen-six<?php else: ?>sixteen-three<?php endif; ?>" style="background-image: url(<?php echo the_field('hero_background_image'); ?>);">
+<div class="hero aspect-ratio-<?php if (is_page('about')):?>sixteen-six<?php elseif (is_page('distributor-alliance')): ?>sixteen-four<?php else: ?>sixteen-three<?php endif; ?>" style="background-image: url(<?php echo the_field('hero_background_image'); ?>);">
   <div class="content">
 
     <?php echo the_field('hero_text'); ?>
