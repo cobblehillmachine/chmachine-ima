012cab836b2bda4f63bad4045d80d3128cc95e10
Margaret Godowns <mgodowns@MardiGrasWrasse.local>
2016-12-18 12:18:38 -0500

    footer stuff

--- wp-content/themes/ima/templates/header.php
+++ wp-content/themes/ima/templates/header.php
@@ -6,6 +6,7 @@
       </a>
     </div>
     <nav class="nav-primary">
+      <img class="mobile-logo" src="/wp-content/themes/ima/assets/images/logo.png">
       <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
