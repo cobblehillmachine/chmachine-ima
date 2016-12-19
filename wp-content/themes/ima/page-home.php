494ba038094e5f4022d8fea0340eefde2154a8a9
Margaret Godowns <mgodowns@MardiGrasWrasse.local>
2016-12-19 12:57:20 -0500

    fixing home image pull in

--- wp-content/themes/ima/page-home.php
+++ wp-content/themes/ima/page-home.php
@@ -69,7 +69,7 @@
       while ( $loop->have_posts() ) : $loop->the_post();
     ?>
     <div class="news-item flex-item">
-      <?php the_post_thumbnail(); ?>
+      <img src="<?php the_field('thumbnail_image'); ?>" alt="">
       <h5><?php the_title(); ?></h5>
       <a href="<?php the_permalink(); ?>">Read&nbsp;More</a>
     </div>
