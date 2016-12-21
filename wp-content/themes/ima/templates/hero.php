6529c9631ab4febfc43826b6ad69e74b3869e1e1
Margaret Godowns <mgodowns@MardiGrasWrasse.local>
2016-12-20 19:55:40 -0500

    adjusting home hero

--- wp-content/themes/ima/templates/hero.php
+++ wp-content/themes/ima/templates/hero.php
@@ -1,4 +1,4 @@
-<div class="hero aspect-ratio-sixteen-nine" style="background-image: url(<?php echo the_field('hero_image'); ?>);">
+<div class="hero aspect-ratio-sixteen-six" style="background-image: url(<?php echo the_field('hero_image'); ?>);">
   <video autoplay muted loop style="width: 100%; height: auto; position: absolute; top: 0; z-index:1">
     <source src="<?php echo the_field('hero_video'); ?>" type="">
     <img src="<?php echo the_field('hero_image'); ?>">
