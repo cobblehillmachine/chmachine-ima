dd110ad962140c99d88b7abbe5aa0edd387540ec
Margaret Godowns <margaret@fuzzco.com>
2017-01-04 20:11:25 -0500

    updates

--- wp-content/themes/ima/gulpfile.js
+++ wp-content/themes/ima/gulpfile.js
@@ -245,7 +245,7 @@ gulp.task('clean', require('del').bind(null, [path.dist]));
 gulp.task('watch', function() {
   browserSync.init({
     files: ['{lib,templates}/**/*.php', '*.php'],
-    proxy: 'test.dev:8888',
+    proxy: 'imaforce.dev',
     snippetOptions: {
       whitelist: ['/wp-admin/admin-ajax.php'],
       blacklist: ['/wp-admin/**']
