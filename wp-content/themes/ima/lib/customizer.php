4c7ffb472c519953c0470fa69e3c396f74e0eb87
neligajb <boone@cobblehilldigital.com>
2017-02-16 16:47:53 -0500

    remove pw strength requirements

--- wp-content/themes/ima/lib/customizer.php
+++ wp-content/themes/ima/lib/customizer.php
@@ -19,3 +19,14 @@ function customize_preview_js() {
   wp_enqueue_script('sage/customizer', Assets\asset_path('scripts/customizer.js'), ['customize-preview'], null, true);
 }
 add_action('customize_preview_init', __NAMESPACE__ . '\\customize_preview_js');
+
+
+/*
+ * Disable password strength checker
+ */
+function disable_strong_pw() {
+  if ( wp_script_is( 'wc-password-strength-meter', 'enqueued' ) ) {
+    wp_dequeue_script( 'wc-password-strength-meter' );
+  }
+}
+add_action( 'wp_print_scripts', __NAMESPACE__ . '\\disable_strong_pw', 100 );
